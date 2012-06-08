221c2a9ab07f66c91f2bdfd2269fcc13108d6671
Angelos Tzotsos <gcpp.kalxas@gmail.com>
2012-06-08 15:10:15 +0300

    Updated fires application script

--- Applications/fires.sh
+++ Applications/fires.sh
@@ -1,14 +1,14 @@
-./SimpleViewer 230_250.img
-./SimpleViewer 242_250.img
-./NDVI 230_250.img ndvi_230.img
+./SimpleViewer FIRE_230_250.img
+./SimpleViewer FIRE_242_250.img
+./NDVI FIRE_230_250.img ndvi_230.img
 ./SimpleViewer ndvi_230.img
-./NDVI 242_250.img ndvi_242.img
+./NDVI FIRE_242_250.img ndvi_242.img
 ./SimpleViewer ndvi_242.img
 ./ImageDifference ndvi_230.img ndvi_242.img diff.img diff_8bit.img
 ./SimpleViewer diff.img
 ./BinaryThreshold diff.img thresh.img -1 -0.1 0 255
 ./SimpleViewer thresh.img 
-./BinaryOpening thresh.img opened_1.img 1
-./SimpleViewer opened_1.img 
-./BinaryOpening thresh.img opened_3.img 3
-./SimpleViewer opened_3.img 
+./BinaryOpening thresh.img opened.img 1
+./SimpleViewer opened.img 
+./BinaryOpening thresh.img final.img 3
+./SimpleViewer final.img 
