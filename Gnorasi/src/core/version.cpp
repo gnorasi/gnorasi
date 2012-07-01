ed0a11be6ecf2d9c23fcf7d90d7a2c61d90a1bf6
Angelos Tzotsos <gcpp.kalxas@gmail.com>
2012-07-02 01:20:13 +0300

    Changed version to 0.1.0. Removed Core processors.

--- Gnorasi/src/core/version.cpp
+++ Gnorasi/src/core/version.cpp
@@ -65,7 +65,7 @@ const std::string VoreenVersion::getCompilerVersion() {
 }
 
 const std::string VoreenVersion::getVersion() {
-    return "4.0";
+    return "0.1.0";
 }
 
 const std::string VoreenVersion::getRevision() {
