493c152ba1f430f0e760fee1321723c41c8d226e
Angelos Tzotsos <gcpp.kalxas@gmail.com>
2012-07-30 03:44:19 +0300

    Added traslation to ERS

--- Utilities/LSD.sh
+++ Utilities/LSD.sh
@@ -5,3 +5,4 @@ gdal_rasterize -a FID -l out /home/kalxas/tmp/gnorasi_build/out.shp /home/kalxas
 ./OneImageBandMath test_8.tif test_bin.tif test2.png "if(b1 > 0.0, 255, 0)"
 gdal_translate -ot Byte test_bin.tif Calw1_10cm_LSD.tif
 rm test*
+gdal_translate -of "ERS" Calw1_10cm_LSD.tif Calw1_10cm_LSD.ers
