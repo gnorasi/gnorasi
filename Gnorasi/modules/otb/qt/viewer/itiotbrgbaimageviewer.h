82a3dc3a81a2cdbfb0b831f58294dbafdac518c5
Ioannis Tsampoulatidis <tsampoulatidis@gmail.com>
2012-12-04 13:41:05 +0200

    added disassembling and assembling functionality

--- Gnorasi/modules/otb/qt/viewer/itiotbrgbaimageviewer.h
+++ Gnorasi/modules/otb/qt/viewer/itiotbrgbaimageviewer.h
@@ -3,6 +3,8 @@
 
 #include <QtCore>
 #include <QWidget>
+#include <QVBoxLayout>
+#include <QSplitter>
 
 #include "itiotbimageviewer.h"
 #include "itiotbimagemanager.h"
@@ -53,8 +55,33 @@ private:
     //! The Zoomable QGLWidget
     ItiOtbRgbaImageWidget *m_pItiOtbRgbaImageWidgetZoom;
 
+    //! The left vertical layout, needed for assembiling and disassembliing purposes
+    QVBoxLayout *m_pvBoxLayoutLeft;
+
+    //! The right vertical layout, needed for assembiling and disassembliing purposes
+    QVBoxLayout *m_pVboxLayoutRight;
+
+    //! The main layout, needed for assembiling and disassembliing purposes
+    QVBoxLayout *m_pMainLayout;
+
+    //! The horizontal splitter;
+    QSplitter *m_pSplitter;
+
+    //! the following labels were taken from the monteverdi viewer
+    //! a label showing a title for the full resolution view
+    QLabel *m_pLabelFullResolution;
+
+    //! a label showing a title for the navigation view
+    QLabel *m_pLabelNavigationView;
+
+    //! a label showing a title for the zoom view
+    QLabel *m_pLabelZoomView;
+
+    //! a label showing a title for the metadata view
+    QLabel *m_pLabelMetadataView;
+
     //! Container widget
-    QWidget *m_pHistogramPlaceHolderWidget;
+    QWidget *m_pMetadataWidget;
 
     
 };
