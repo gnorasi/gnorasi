f15e13e49260dd7bc9fe734d0a76515600044db1
Ioannis Tsampoulatidis <tsampoulatidis@gmail.com>
2013-01-10 13:38:40 +0200

    added nostrething and standard rendering functionality to the command panel

--- Gnorasi/modules/otb/qt/viewer/commands/commandcontrastenhancementlinearxperc.cpp
+++ Gnorasi/modules/otb/qt/viewer/commands/commandcontrastenhancementlinearxperc.cpp
@@ -1,6 +1,42 @@
+/********************************************************************************
+ *                                                                              *
+ * GNORASI - The Knowlegde-Based Remote Sensing Engine                          *
+ *                                                                              *
+ * Language:  C++                                                               *
+ *                                                                              *
+ * Copyright (c) Ioannis Tsampoulatidis <itsam@iti.gr>. All rights reserved. 	*
+ * Copyright (c) Informatics and Telematics Institute                           *
+ *	  Centre for Research and Technology Hellas. All rights reserved.           *
+ * Copyright (c) National Technical University of Athens. All rights reserved.	*
+ *                                                                              *
+ *                                                                              *
+ * This file is part of the GNORASI software package. GNORASI is free           *
+ * software: you can redistribute it and/or modify it under the terms           *
+ * of the GNU General Public License version 2 as published by the              *
+ * Free Software Foundation.                                                    *
+ *                                                                              *
+ * GNORASI is distributed in the hope that it will be useful,                   *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of               *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the                 *
+ * GNU General Public License for more details.                                 *
+ *                                                                              *
+ * You should have received a copy of the GNU General Public License            *
+ * in the file "LICENSE.txt" along with this program.                           *
+ * If not, see <http://www.gnu.org/licenses/>.                                  *
+ *                                                                              *
+ ********************************************************************************/
+#include <QtCore>
+#include <QtGui>
+
 #include "commandcontrastenhancementlinearxperc.h"
 
+#include "../widgets/vector/itiotbvectorimageviewer.h"
+#include "../models/itiotbVectorImageModel.h"
+#include "../vector_globaldefs.h"
+
 using namespace itiviewer;
+using namespace otb;
+
 
 CommandContrastEnhancementLinearXPerc::CommandContrastEnhancementLinearXPerc(ItiOtbVectorImageViewer *viewer, QObject *parent) :
     m_upperQuantile(2.0), m_lowerQuantile(2.0), m_pItiOtbVectorImageViewer(viewer), Command(parent)
@@ -8,6 +44,16 @@ CommandContrastEnhancementLinearXPerc::CommandContrastEnhancementLinearXPerc(Iti
 }
 
 void CommandContrastEnhancementLinearXPerc::execute(){
-    //! TODO
-    //! apply the filter here
+    VectorImageModel *vModel = qobject_cast<VectorImageModel*>(m_pItiOtbVectorImageViewer->model());
+    if(!vModel)
+        return;
+
+    RenderingFilterType *filter = vModel->filter();
+
+    RenderingFunctionType *renderer = dynamic_cast<RenderingFunctionType*>(NoStretchRenderingFunctionType::New().GetPointer());
+
+    if(filter)
+        filter->SetRenderingFunction(renderer);
+
+    m_pItiOtbVectorImageViewer->draw();
 }
