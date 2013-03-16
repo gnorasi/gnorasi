0f4d2345905e83ebac26426fdaefaae0c0d30d20
Angelos Tzotsos <gcpp.kalxas@gmail.com>
2013-03-16 22:14:13 +0200

    careful with capital letters...

--- Gnorasi/modules/otb/otbmodule.cpp
+++ Gnorasi/modules/otb/otbmodule.cpp
@@ -52,7 +52,7 @@
 #include "processors/BasicFilters/otbgrayscaleopeningfilterprocessor.h"
 #include "processors/BasicFilters/otbbinaryclosingfilterprocessor.h"
 #include "processors/BasicFilters/otbgrayscaleclosingfilterprocessor.h"
-#include "processors/BasicFilters/otbleeImageFilterProcessor.h"
+#include "processors/BasicFilters/otbleeimagefilterprocessor.h"
 #include "processors/OBIA/otblabelimagetolabelmapprocessor.h"
 #include "processors/OBIA/otbshapeattributeslabelmapprocessor.h"
 #include "processors/OBIA/otbbandstatisticsattributeslabelmapprocessor.h"
